df188d9114d1f2f617a88eb8a3f67c407142b16c
Gustavo Mauricio <gmauricio2072gmail.com>
2021-05-24 19:36:18 -0300

    Inserir nova chave e valor e imprimi todas infos do object na tela

---
+++ INTRODUÇÃO/BLOCO_04/DIA_04/exercises_02.js
@@ -0,0 +1,13 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  info.Recorrente = "Sim";
+
+  for(let key in info){
+      console.log(key + " " + info[key]);
+  };
+
+
