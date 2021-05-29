343a70271c6ab7169d87af90d1a2961f69cea93a
Gustavo Mauricio <gmauricio2072gmail.com>
2021-05-29 13:44:45 -0300

    Adiciona verificacao da diagonal inferior esquerda

--- INTRODUÇÃO/BLOCO_04/DIA_03/AULA_AOVIVO/queensAttack.js
+++ INTRODUÇÃO/BLOCO_04/DIA_03/AULA_AOVIVO/queensAttack.js
@@ -23,13 +23,31 @@ if((linhaOponente === linhaAtualRainha) && (colunaOponente === attackBemSucedido
 }
 }
 
-for( let iSupEsq = 1; iSupEsq < 8 iSupEsq += 1){
+for( let iSupEsq = 1; iSupEsq < 8; iSupEsq += 1){
 	let linhaAtualRainha = linhaRainha + iSupEsq;
 	let colunaAtualRainha = colunaRainha - iSupEsq;
 
 	if((linhaAtualRainha > 8) || (colunaAtualRainha < 1)){
 		break;
 	}
-	
+
+	if((linhaOponente === linhaAtualRainha) && (colunaOponente === attackBemSucedido)){
+		attackBemSucedido = true;
+
 }
+
+for(let iInfEsq = 1; iInfEsq < 8; iInfEsq += 1){
+	let linhaAtualRainha = linhaRainha - iInfEsq;
+	let colunaAtualRainha = colunaRainha - iInfEsq;
+
+	if((linhaAtualRainha < 1) || (colunaAtualRainha < 1)){
+		break;
+	}
+
+	if((linhaOponente === linhaAtualRainha) && (colunaOponente === attackBemSucedido)){
+		attackBemSucedido = true;
+
+}
+
+for(let iInfDir)
 console.log("Ataque bem sucedido?" , attackBemSucedido)
