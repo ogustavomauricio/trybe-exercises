2cc025364483fae1b7fc2366806d269a6f5cf963
Gustavo Mauricio <gmauricio2072gmail.com>
2021-05-29 13:33:15 -0300

    Verifica a diagonal superior direita

--- INTRODUÇÃO/BLOCO_04/DIA_03/AULA_AOVIVO/queensAttack.js
+++ INTRODUÇÃO/BLOCO_04/DIA_03/AULA_AOVIVO/queensAttack.js
@@ -1,13 +1,30 @@
 let linhaRainha = 4;
 let colunaRainha = "D";
 
-let linhaOponente = 4;
-let colunaOponente = "G";
+let linhaOponente = 9;
+let colunaOponente = "e";
 
 let attackBemSucedido = false;
+
 
 if((linhaRainha === linhaOponente) || (colunaRainha === colunaOponente)){
 	attackBemSucedido = true;
 }
-    
+
+for( let iSupDir = 1; iSupDir < 8 ;iSupDir += 1){
+let linhaAtualRainha = linhaRainha + iSupDir;
+let colunaAtualRainha = colunaRainha + iSupDir;
+
+if((linhaAtualRainha > 8) || (colunaAtualRainha > 8)){
+	break;
+}
+if((linhaOponente === linhaAtualRainha) && (colunaOponente === attackBemSucedido)){
+	attackBemSucedido = true;
+}
+}
+
+for( let iSupEsq = 1; iSupEsq < 8 iSupEsq += 1){
+	let linhaAtualRainha = linhaRainha + iSupEsq;
+	let colunaAtualRainha = colunaRainha - iSupEsq;
+}
 console.log("Ataque bem sucedido?" , attackBemSucedido)
