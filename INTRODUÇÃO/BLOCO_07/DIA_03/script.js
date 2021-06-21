7ecf08c978a3346dbcd643ea548178e3218cf821
Gustavo Mauricio <gmauricio207@gmail.com>
2021-06-21 20:53:30 -0300

    Código alterado e teste criado para saída ser igual ao result

--- INTRODUÇÃO/BLOCO_07/DIA_03/script.js
+++ INTRODUÇÃO/BLOCO_07/DIA_03/script.js
@@ -235,14 +235,23 @@
 
 // Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.
 
+const assert = require('assert');
+
 const greetPeople = (people) => {
     let greeting = 'Hello ';
+   
   
     for (const person in people) {
-      greeting += people[person];
+     const result = (`${greeting} ${people[person]}`)
+       
     }
-    return greeting;
+    // console.log(result)
+    return result;
+
   };
   
   const parameter = ['Irina', 'Ashleigh', 'Elsa'];
   const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
+  const output = greetPeople(parameter);
+
+  assert.strictEqual(output,result);
