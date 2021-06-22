b8a428e0e2bcb86a1b3509af9eae4604ca99d5fc
Gustavo Mauricio <gmauricio207@gmail.com>
2021-06-21 22:13:53 -0300

    Parte 3 - exercício 4 com código errado

--- INTRODUÇÃO/BLOCO_07/DIA_03/script.js
+++ INTRODUÇÃO/BLOCO_07/DIA_03/script.js
@@ -294,21 +294,37 @@
 
 // Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.
 
-const assert = require('assert');
+// const assert = require('assert');
 
-const greaterThanTen = (array) => {
-    const results = [];
-    for (let index = 0; index < array.length; index += 1) {
-      if (array[index] > 10) {
-         results.push(array[index]);
-      }
+// const greaterThanTen = (array) => {
+//     const results = [];
+//     for (let index = 0; index < array.length; index += 1) {
+//       if (array[index] > 10) {
+//          results.push(array[index]);
+//       }
       
-    }
-    return results;
-  };
+//     }
+//     return results;
+//   };
   
-  const parameter = [4, 10, 32, 9, 21];
-  const result = [32, 21];
-  const output = greaterThanTen(parameter);
+//   const parameter = [4, 10, 32, 9, 21];
+//   const result = [32, 21];
+//   const output = greaterThanTen(parameter);
 
-  assert.deepStrictEqual(output,result);
+//   assert.deepStrictEqual(output,result);
+
+// =======================================================================================
+
+//Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.
+
+function secondThirdSmallest(array) {
+  let results = []
+  array.sort(function (x, y) {
+      return x + y;
+  });
+  results = [array[1], array[2]];
+  return results;
+};
+
+const parameter = [4, 10, 32, 9, 21, 90, 5, 11, 8, 6];
+const result = [5, 6];
