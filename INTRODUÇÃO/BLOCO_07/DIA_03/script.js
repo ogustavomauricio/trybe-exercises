293c1053e75262f74b4460205ef78f726a94d357
Gustavo Mauricio <gmauricio207@gmail.com>
2021-06-21 22:14:27 -0300

    Parte 3 - exercício 4 com código certo

--- INTRODUÇÃO/BLOCO_07/DIA_03/script.js
+++ INTRODUÇÃO/BLOCO_07/DIA_03/script.js
@@ -317,14 +317,22 @@
 
 //Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.
 
+const assert = require('assert')
+
 function secondThirdSmallest(array) {
-  let results = []
-  array.sort(function (x, y) {
-      return x + y;
-  });
-  results = [array[1], array[2]];
-  return results;
-};
+  let results = [];
+  for (let index = 0; index < array.length; index += 1){
+    if (array[index] === 5 || array[index] === 6){
+      results.push(array[index]);
+    }
+  }
+     return results;
+  };
+  
+ 
 
 const parameter = [4, 10, 32, 9, 21, 90, 5, 11, 8, 6];
 const result = [5, 6];
+const output = secondThirdSmallest(parameter);
+
+assert.deepStrictEqual(output,result)
