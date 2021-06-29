7d3745fa850cc520e90003c228de1aeaef4b53a0
Gustavo Mauricio <gmauricio207@gmail.com>
2021-06-29 20:05:36 -0300

    Exercicio 6 do dia 8.5

--- INTRODUÇÃO/BLOCO_08/DIA_05/script.js
+++ INTRODUÇÃO/BLOCO_08/DIA_05/script.js
@@ -102,15 +102,35 @@
 //5 - Escreva a função swap , que dado um array de 3 elementos, retorna um novo array com o primeiro e terceiro elementos trocados. Detalhe: você precisa fazer essa função gastando 1 linha só:
 // Dica: use array destructuring .
 
+// const assert = require('assert');
+
+// const myList = [1, 2, 3];
+
+// // escreva swap abaixo
+// const swap = ([a,b,c]) => [c,b,a] 
+
+// const swappedList = swap(myList);
+
+// assert.strictEqual(swappedList[0], 3);
+// assert.strictEqual(swappedList[1], 2);
+// assert.strictEqual(swappedList[2], 1);
+
+//=======================================================================================
+
+//6 - Suponha que você esteja lidando com carros e, da forma como o problema lhe foi entregue, cada carro é modelado como um array. Porém, essa modelagem está baixo nível. Cria uma função toObject que, dada uma lista, retorna um objeto representando o carro:
+// Dica: use array destructuring e abbreviation object literal .
+
 const assert = require('assert');
 
-const myList = [1, 2, 3];
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
 
-// escreva swap abaixo
-const swap = ([a,b,c]) => [c,b,a] 
+// escreva toObject abaixo
 
-const swappedList = swap(myList);
 
-assert.strictEqual(swappedList[0], 3);
-assert.strictEqual(swappedList[1], 2);
-assert.strictEqual(swappedList[2], 1);
+
+
+ assert.deepStrictEqual(toObject(palio), { name: 'Palio', brand: 'Fiat', year: 2019 });
+assert.deepStrictEqual(toObject(shelbyCobra), { name: 'Shelby Cobra', brand: 'Ford', year: 1963 });
+assert.deepStrictEqual(toObject(chiron), { name: 'Chiron', brand: 'Bugatti', year: 2016 });
