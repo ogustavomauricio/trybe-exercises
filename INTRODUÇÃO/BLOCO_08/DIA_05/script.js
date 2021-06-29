2b87b6a0ef7e8cf9d831692565f979b110fd5fda
Gustavo Mauricio <gmauricio207@gmail.com>
2021-06-29 18:29:59 -0300

    Exercicio 3 do dia 8.5

--- INTRODUÇÃO/BLOCO_08/DIA_05/script.js
+++ INTRODUÇÃO/BLOCO_08/DIA_05/script.js
@@ -11,15 +11,44 @@
 //   assert.strictEqual(rectangleArea(), rectangle[0] * rectangle[1]); // altere a chamada da funcao rectangleArea
 // });
 
+//=========================================================
+// const assert = require('assert');
+
+// // escreva sum abaixo
+// function sum(...args) {
+//     return args.reduce((acc, current) => acc + current, 0);
+// }
+
+// assert.strictEqual(sum(), 0);
+// assert.strictEqual(sum(1), 1);
+// assert.strictEqual(sum(1, 2), 3);
+// assert.strictEqual(sum(1, 2, 3), 6);
+// assert.strictEqual(sum(1, 2, 3, 4), 10);
+
+//================================================================
+//3 - Escreva a função personLikes , que dado um objeto de parâmetro que representa uma pessoa, retorna todos os gostos daquela pessoa, conforme mostrado abaixo:
+// Dica: use object destructuring.
+
 const assert = require('assert');
 
-// escreva sum abaixo
-function sum(...args) {
-    return args.reduce((acc, current) => acc + current, 0);
-}
+const alex = {
+  name: 'Alex',
+  age: 26,
+  likes: ['fly fishing'],
+  nationality: 'Australian',
+};
 
-assert.strictEqual(sum(), 0);
-assert.strictEqual(sum(1), 1);
-assert.strictEqual(sum(1, 2), 3);
-assert.strictEqual(sum(1, 2, 3), 6);
-assert.strictEqual(sum(1, 2, 3, 4), 10);
+const gunnar = {
+  name: 'Gunnar',
+  age: 30,
+  likes: ['hiking', 'scuba diving', 'taking pictures'],
+  nationality: 'Icelandic',
+};
+
+// complete a assinatura da função abaixo
+
+
+const personLikes = () => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+
+assert.strictEqual(personLikes(alex), 'Alex is 26 years old and likes fly fishing.');
+assert.strictEqual(personLikes(gunnar), 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.');
