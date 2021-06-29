88aafc851bf6e3cff730fc24e690f2ce29a62863
Gustavo Mauricio <gmauricio207@gmail.com>
2021-06-29 18:59:03 -0300

    Exercicio 4 do dia 8.5

--- INTRODUÇÃO/BLOCO_08/DIA_05/script.js
+++ INTRODUÇÃO/BLOCO_08/DIA_05/script.js
@@ -29,26 +29,69 @@
 //3 - Escreva a função personLikes , que dado um objeto de parâmetro que representa uma pessoa, retorna todos os gostos daquela pessoa, conforme mostrado abaixo:
 // Dica: use object destructuring.
 
+// const assert = require('assert');
+
+// const alex = {
+//   name: 'Alex',
+//   age: 26,
+//   likes: ['fly fishing'],
+//   nationality: 'Australian',
+// };
+
+// const gunnar = {
+//   name: 'Gunnar',
+//   age: 30,
+//   likes: ['hiking', 'scuba diving', 'taking pictures'],
+//   nationality: 'Icelandic',
+// };
+
+// // complete a assinatura da função abaixo
+
+// const personLikes = ({name, age, likes}) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+
+
+// assert.strictEqual(personLikes(alex), 'Alex is 26 years old and likes fly fishing.');
+// assert.strictEqual(personLikes(gunnar), 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.');
+
+//================================================================================
+
+//4 - Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
+// Dica: use object destructuring .
+
 const assert = require('assert');
 
-const alex = {
-  name: 'Alex',
-  age: 26,
-  likes: ['fly fishing'],
-  nationality: 'Australian',
-};
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
 
-const gunnar = {
-  name: 'Gunnar',
-  age: 30,
-  likes: ['hiking', 'scuba diving', 'taking pictures'],
-  nationality: 'Icelandic',
-};
-
-// complete a assinatura da função abaixo
-
-const personLikes = ({name, age, likes}) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+// escreva filterPeople abaixo
 
 
-assert.strictEqual(personLikes(alex), 'Alex is 26 years old and likes fly fishing.');
-assert.strictEqual(personLikes(gunnar), 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.');
+const filteredPeople = filterPeople(people);
+
+assert.deepStrictEqual(filteredPeople[0], { name: 'Nicole', bornIn: 1992, nationality: 'Australian' });
+assert.deepStrictEqual(filteredPeople[1], { name: 'Toby', bornIn: 1901, nationality: 'Australian' });
