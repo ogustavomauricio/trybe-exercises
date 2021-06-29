3ec4d63b4393d0fdd7f8ff086f8f50ffa437d8e8
Gustavo Mauricio <gmauricio207@gmail.com>
2021-06-29 18:13:25 -0300

    Exercicio 2 do dia 8.5

--- INTRODUÇÃO/BLOCO_08/DIA_05/script.js
+++ INTRODUÇÃO/BLOCO_08/DIA_05/script.js
@@ -1,12 +1,22 @@
 const assert = require('assert');
 
-const rectangleArea = (width, height) => width * height;
+// const rectangleArea = (width, height) => width * height;
 
-const rectangle1 = [1, 2];
-const rectangle2 = [3, 5];
-const rectangle3 = [6, 9];
-const rectangles = [rectangle1, rectangle2, rectangle3];
+// const rectangle1 = [1, 2];
+// const rectangle2 = [3, 5];
+// const rectangle3 = [6, 9];
+// const rectangles = [rectangle1, rectangle2, rectangle3];
 
-rectangles.forEach((...rectangle) => {
-  assert.strictEqual(rectangleArea(), rectangle[0] * rectangle[1]); // altere a chamada da funcao rectangleArea
-});
+// rectangles.forEach((...rectangle) => {
+//   assert.strictEqual(rectangleArea(), rectangle[0] * rectangle[1]); // altere a chamada da funcao rectangleArea
+// });
+
+const assert = require('assert');
+
+// escreva sum abaixo
+
+assert.strictEqual(sum(), 0);
+assert.strictEqual(sum(1), 1);
+assert.strictEqual(sum(1, 2), 3);
+assert.strictEqual(sum(1, 2, 3), 6);
+assert.strictEqual(sum(1, 2, 3, 4), 10);
