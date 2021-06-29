f5d344da6a780c2e9ee4558938da226fbf1391e2
Gustavo Mauricio <gmauricio207@gmail.com>
2021-06-29 18:12:23 -0300

    Exercicio 1 do dia 8.5 - Resolvido

--- INTRODUÇÃO/BLOCO_08/DIA_05/script.js
+++ INTRODUÇÃO/BLOCO_08/DIA_05/script.js
@@ -7,6 +7,6 @@ const rectangle2 = [3, 5];
 const rectangle3 = [6, 9];
 const rectangles = [rectangle1, rectangle2, rectangle3];
 
-rectangles.forEach((rectangle) => {
+rectangles.forEach((...rectangle) => {
   assert.strictEqual(rectangleArea(), rectangle[0] * rectangle[1]); // altere a chamada da funcao rectangleArea
 });
