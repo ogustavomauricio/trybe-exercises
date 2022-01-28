3a9041e4d7a09ebd3e1bf17cb52b32d461b53ad7
Gustavo Mauricio <gmauricio207@gmail.com>
2022-01-28 18:56:01 -0300

    Codigo refatorado seguindo os conceitos de SOLID

--- BACK-END/SOLID/exercises/plants.js
+++ BACK-END/SOLID/exercises/plants.js
@@ -20,9 +20,15 @@ const defaultPlants = [
   
   let createdPlants = 0;
   
+
+  const calculeteWaterFrequency = (needsSun, size, origin) => {
+    needsSun ? size *  0.77 + (origin === 'Brazil' ? 8 : 7)
+    : (size / 2) *  1.33 + (origin === 'Brazil' ? 8 : 7)
+  } 
+
+
   const initPlant = (id, breed, needsSun, origin, specialCare, size) => {
-    const waterFrequency = needsSun ? size *  0.77 + (origin === 'Brazil' ? 8 : 7)
-      : (size / 2) *  1.33 + (origin === 'Brazil' ? 8 : 7)
+   const waterFrequency = calculeteWaterFrequency(needsSun, size, origin);
     const newPlant = {
       id,
       breed,
@@ -37,14 +43,9 @@ const defaultPlants = [
     return newPlant;
   };
   
-  const savePlants = () => {
-    const plants = JSON.stringify(defaultPlants);
-    localStorage.setItem("plants", plants);
-  };
   
   const getPlants = () => {
-    const plants = JSON.parse(localStorage.getItem("plants"));
-    return plants;
+    return defaultPlants;
   };
   
   const getPlantById = (id) => {
@@ -52,21 +53,14 @@ const defaultPlants = [
   };
   
   const removePlantById = (id) => {
-    const newPlants = defaultPlants.filter((plant) => plant.id !== id);
-    localStorage.setItem("plants", JSON.stringify(newPlants));
+    return defaultPlants.filter((plant) => plant.id !== id);
   };
   
   const getPlantsThatNeedsSunWithId = (id) => {
-    const filteredPlants = defaultPlants.filter((plant) => {
-      if (plant.needsSun && plant.id === id) {
-        if (plant.specialCare.waterFrequency > 2) {
-          return plant;
-        }
-      }
-    });
-    localStorage.setItem("plants", JSON.stringify(filteredPlants));
-    return filteredPlants;
-  };
+    return defaultPlants.filter((plant) => {
+    return needsSun(plant) && plant.id === id;
+      })
+    };
   
   const editPlant = (plantId, newPlant) => {
     return defaultPlants.map((plant) => {
@@ -78,10 +72,18 @@ const defaultPlants = [
   };
   
   const createNewPlant = (plant) => {
-    const mappedPlant = initPlant({ ...plant });
-    defaultPlants.push(mappedPlant);
     createdPlants++;
-    localStorage.setItem("createdPlants", String(createdPlants));
-    localStorage.setItem("plants", JSON.stringify(defaultPlants));
+    const mappedPlant = initPlant( createdPlants, { ...plant });
+    defaultPlants.push(mappedPlant);
+    
     return defaultPlants;
+  };
+
+  module.exports = {
+    createNewPlant,
+    editPlant,
+    getPlantsThatNeedsSunWithId,
+    removePlantById,
+    getPlantById,
+    getPlants,
   };
